2899688456cf69c7a92bd1e1e2d0fe3d3add0321
Tobias Stevenson <codenamekt@gmail.com>
2015-03-06 13:55:46 -0600

    Adding more file loading.

--- frontend/main.py
+++ frontend/main.py
@@ -10,7 +10,7 @@ __pyver__ = ['2.7.4']
 __created__ = ['3.3.2012']
 __authors__ = ['Toby <codenamekt@gmail.com>']
 __copyright__ = ['GNU GPL v3 <http://www.gnu.org/licenses/gpl.html>']
-__shortdoc__ = 'Package initialization.'
+__shortdoc__ = 'frontend'
 
 import os
 import sys
@@ -22,15 +22,37 @@ LOCAL_FILE = os.path.realpath(__file__)
 LOCAL_DIR = os.path.dirname(LOCAL_FILE)
 
 
+class FileParse:
+    # Todo: Read write from monogdb
+    def __init__(self, abs_path, index):
+        self.file_path = os.path.join(abs_path, index)
+        self.data = self.refresh()
+
+    def read(self):
+        """
+        Returns data from reading file.
+        :rtype : str
+        """
+        with open(self.file_path, 'r') as f:
+            data = f.read()
+            f.close()
+
+        return data
+
+
 class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        # Setting to allow local file.
+        self.ui.browser.settings().setAttribute(QWebSettings.LocalContentCanAccessRemoteUrls, True)
+        self.local_file = None
 
-    def load_preview_from_local(self, abs_path, index):
+    def load_local_file(self, abs_path=None, index=None):
         """
         Loads a file into the previewer tab.
+        :type self: MainWindow
         :rtype : boolean
 
         :param abs_path
@@ -41,11 +63,15 @@ class MainWindow(QtGui.QMainWindow):
         :type index: str
         Filename to open in preview tab.
         """
-        f = open(os.path.join(abs_path, index), 'r')
-        html = f.read()
-        f.close()
-        self.ui.browser.settings().setAttribute(QWebSettings.LocalContentCanAccessRemoteUrls, True)
-        self.ui.browser.setHtml(html)
+        if abs_path and index:
+            self.local_file = FileParse(abs_path, index)
+        elif self.local_file:
+            pass
+        else:
+            # Todo: Change to warning dialog
+            raise NotImplementedError("Change to dialog")
+
+        self.ui.browser.setHtml(self.local_file.data)
 
         return True
 
@@ -53,6 +79,6 @@ if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     main = MainWindow()
     abs_index_path = os.path.join(LOCAL_DIR, "site")
-    main.load_preview_from_local(abs_path=abs_index_path, index="index.html")
+    main.load_local_file(abs_path=abs_index_path, index="index.html")
     main.show()
     sys.exit(app.exec_())
